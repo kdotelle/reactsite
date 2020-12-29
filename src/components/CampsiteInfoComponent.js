568c27cfd48fbe3d548cbb38c31ed90a6a213ef6
Kristen Lloyd <lloyd.kristen91@gmail.com>
2020-12-28 22:33:33 -0500

    convert components to functional components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -8,73 +8,72 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
 //the CampsiteInfo class is used to make the CampsiteInfo component in DirectoryComponents.js
 //class doesn't do anything w/o the Component
-class CampsiteInfo extends React.Component {
-    render() {
-        if (this.props.clickedCampsite) {
-             return (
-                 <div className="container">
-                    <div className="row"> 
-                        {this.renderCampsite(this.props.clickedCampsite)}
-                        {this.renderComments(this.props.clickedCampsite.comments)}
-                    </div>
+
+function CampsiteInfo(props) {
+    if (props.clickedCampsite) {
+            return (
+                <div className="container">
+                <div className="row"> 
+                    <RenderCampsite campsite={props.clickedCampsite} />
+                    <RenderComments comments={props.clickedCampsite.comments} />
                 </div>
-             )
-           } else {
-               return (
-                   <div />
-               )
-           };
-         }
+            </div>
+            )
+        } else {
+            return (
+                <div />
+            )
+        };
+    }
 //props has the same name as the state object
 //when the state changes that change in data is passed to the components through the props
-        renderCampsite(campsite) {
-            if (this.props.clickedCampsite) {
-                return (
-                    <div className="row col-md-5 m-1"> 
-                       <Card>
-                           <CardImg top width="100%" src={this.props.clickedCampsite.image} alt={this.props.clickedCampsite.name} />
-                           <CardBody>
-                               <CardTitle>{this.props.clickedCampsite.name}</CardTitle>
-                               <CardText>{this.props.clickedCampsite.description}</CardText>
-                           </CardBody>
-                       </Card>
-                    </div>
-                )
-              } else {
-                  return (
-                      <div />
-                  )
-              };
-
-        }
+function RenderCampsite({campsite}) {
+    if (campsite) {
+        return (
+            <div className="row col-md-5 m-1"> 
+                <Card>
+                    <CardImg top width="100%" src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        )
+        } else {
+            return (
+                <div />
+            )
+        };
+    }
 //clickedCampsite attribute was defined in DirectorComponents
 //clickedCampsite state is being passed through as a prop
 //map calls the function for EACH element in the array and returns a new array
-        renderComments(comments) {
-            if (this.props.clickedCampsite.comments) {
+function RenderComments({comments}) {
+    if (comments) {
+        return (
+            <div className="col-md-5 m-1">
+                <h4> Comments</h4>
+                {comments.map(comments => {
                 return (
-                    <div className="col-md-5 m-1">
-                      <h4> Comments</h4>
-                      {this.props.clickedCampsite.comments.map(comments => {
-                        return (
-                            <ListGroup>
-                                <ListGroupItem key={comments.id}> 
-                                    <ListGroupItemHeading>{comments.text}</ListGroupItemHeading> 
-                                    <ListGroupItemText>--{comments.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comments.date)))}</ListGroupItemText>
-                                </ListGroupItem>
-                            </ListGroup>
-                        );
-                      })
-                    }
-                    </div>
-                  )
-            } else {
-                return (
-                    <div />
-                )
-            };
-        }     
-}
+                    <ListGroup>
+                        <ListGroupItem key={comments.id}> 
+                            <ListGroupItemHeading>{comments.text}</ListGroupItemHeading> 
+                            <ListGroupItemText>--{comments.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comments.date)))}</ListGroupItemText>
+                        </ListGroupItem>
+                    </ListGroup>
+                );
+                })
+            }
+            </div>
+            )
+    } else {
+        return (
+            <div />
+        )
+    };
+}     
+
 
 
 
--- src/components/DirectoryComponents.js
+++ src/components/DirectoryComponents.js
@@ -1,37 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
+//functional components always receive any data passed to them as properties of a single props argument
+function RenderDirectoryItem({campsite, onClick}) {
+    return (
+        <Card onClick={() => onClick(campsite.id)}>
+            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+            <CardImgOverlay>
+                <CardTitle>{campsite.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
+}
 
-class Directory extends Component {
+function Directory(props) {
 
 //setState updates the interface in response to an event handler
 //when a new campsite is selected the state changes to that campsite
 
-
-    render() {
-        const directory = this.props.campsites.map(campsite => {
-            return (
-                <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.props.onClick(campsite.id)}>
-                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{campsite.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div>
-            );
-        });
+    const directory = props.campsites.map(campsite => {
+        return (
+            <div key={campsite.id} className="col-md-5 m-1">
+                <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
+            </div>
+        );
+    });
 //this.state is what's currently on the screen
 //made the CampsiteInfo component from the CampsiteInfo class in the CampsiteInfoComponent.js file
 //Component is powered by the class
-        return (
-            <div className="container">
-                <div className="row">
-                    {directory}
-                </div>
+    return (
+        <div className="container">
+            <div className="row">
+                {directory}
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 
