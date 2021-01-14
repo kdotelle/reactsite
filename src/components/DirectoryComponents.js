509f7f7c4862c7b30548f13a4ec8f13f299e9ee8
Kristen Lloyd <lloyd.kristen91@gmail.com>
2021-01-13 23:13:23 -0500

    setup action creators. json-server fetch

--- src/components/DirectoryComponents.js
+++ src/components/DirectoryComponents.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl'
 
 //functional components always receive any data passed to them as properties of a single props argument
 function RenderDirectoryItem({campsite}) {
     return (
         <Card>
             <Link to={`/directory/${campsite.id}`}>
-                <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                <CardImg width="100%" src={baseUrl + campsite.image} alt={campsite.name} />
                 <CardImgOverlay>
                     <CardTitle>{campsite.name}</CardTitle>
                 </CardImgOverlay>
@@ -18,9 +19,6 @@ function RenderDirectoryItem({campsite}) {
 }
 
 function Directory(props) {
-
-//setState updates the interface in response to an event handler
-//when a new campsite is selected the state changes to that campsite
 
     const directory = props.campsites.campsites.map(campsite => {
         return (
